0dee4586c7b645dbf21c68fdb5fc083bbf29ec0f
Matt Goodall <matt.goodall@gmail.com>
2014-04-10 11:01:09 +0100

    Allow single package installation.

--- install.sh
+++ install.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 
+package=$1
+if [ "$package" = "" ]; then
+    package=`ls -d */`
+fi
+
 git submodule update --init
 git submodule foreach git pull origin master
 git submodule foreach git submodule update --init
 
-stow -v -t ~ -R `ls -d */`
+stow -v -t ~ -R $package
